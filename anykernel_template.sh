a59d4cf83ef47ded5b6d8f8a591282a83863f31c
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-03-14 23:43:40 +0530

    chore: rename zip and anykernel for KSU Next branch

--- anykernel_template.sh
+++ anykernel_template.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=SukiSU_Ultra_RMX3031
+kernel.string=KSU_Next_RMX3031
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -38,12 +38,12 @@ PATCH_VBMETA_FLAG=auto;
 
 ui_print " ";
 ui_print "************************************";
-ui_print "        SukiSU_Ultra_RMX3031        ";
+ui_print "        KSU_Next_RMX3031          ";
 ui_print "************************************";
 ui_print " ";
 ui_print "  Kernel Source: ManshuTyagi";
 ui_print "  Root Implementation: xCaptaiN09";
-ui_print "  SukiSU Ultra + SuSFS v2.0.0";
+ui_print "  KernelSU Next";
 ui_print " ";
 ui_print "************************************";
 ui_print " ";
