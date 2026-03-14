2ae6f785558c16afe475a7124501aab6691c4a70
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-03-15 00:45:21 +0530

    feat: add SuSFS v2.0.0 via KernelSU Next legacy_susfs branch

--- init/main.c
+++ init/main.c
@@ -776,6 +776,8 @@ asmlinkage __visible void __init start_kernel(void)
 	}
 
 	/* Do the rest non-__init'ed, we're now alive */
+	extern void susfs_init(void);
+	susfs_init();
 	rest_init();
 
 	prevent_tail_call_optimization();
