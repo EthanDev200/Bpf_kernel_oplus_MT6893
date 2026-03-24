b252bd76f20afce905f7df7e162c60aaa1f93b6b
xCaptaiN09 <116649374+xCaptaiN09@users.noreply.github.com>
2026-03-24 01:53:52 +0000

    anykernel: add init.rc cgroup patch for Color OS 16 / Realme UI

--- anykernel_template.sh
+++ anykernel_template.sh
@@ -54,5 +54,9 @@ ui_print " ";
 # boot install
 split_boot;
 
+# init.rc patch for Color OS 16 / Realme UI cgroup fix
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
 flash_boot;
 ## end boot install
